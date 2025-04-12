8f302106695ba917db30ac9070a8aa463f046407
jinpain <savpavel2000@gmail.com>
2025-04-12 22:46:01 +0500

    patients table edited

--- migrations/20250410154856_add_patients_table.sql
+++ migrations/20250410154856_add_patients_table.sql
@@ -1,3 +1,4 @@
+-- Active: 1744043592357@@127.0.0.1@5432@patients_db
 -- +goose Up
 -- +goose StatementBegin
 CREATE TABLE patients (
@@ -7,7 +8,12 @@ CREATE TABLE patients (
     middle_name VARCHAR(100),
     birth_date DATE NOT NULL,
     phone_number VARCHAR(11),
-    email VARCHAR(64)
+    email VARCHAR(64),
+    deleted_ts TIMESTAMP DEFAULT NULL,
+    deleted_by VARCHAR(64) DEFAULT NULL,
+    updated_ts TIMESTAMP DEFAULT NULL,
+    updated_by VARCHAR(64) DEFAULT NULL,
+    version int DEFAULT 0
 );
 
 -- +goose StatementEnd
